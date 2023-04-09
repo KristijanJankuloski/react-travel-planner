0dd0d4a49740a394a1ee412e70462a7472281819
Kristijan <jankuloski.k@gmail.com>
2023-04-09 19:27:12 +0200

    Minor corrections

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
-import '/favicon.svg';
+import icon from '../../assets/img/favicon.svg';
 import './Header.css';
 
 function Header() {
     return ( <header className='text-black bg-light'>
         <div className="container d-flex justify-content-around align-items-center">
-            <a className="" href="/"><img src="/favicon.svg" className='travel-img' alt="travel icon" /></a>  
+            <a className="" href="/"><img src={icon} className='travel-img' alt="travel icon" /></a>  
             <h2 className='display-2'>Travel planner</h2>
         </div>
     </header> );
